ccd27249d7b5a25d5df3aabbeea05d79c34b9931
Evan Feenstra <evan@evanfeenstra.com>
2020-06-09 17:03:21 -0700

    photo_url

--- interfaces.ts
+++ interfaces.ts
@@ -30,6 +30,7 @@ interface Contact {
     pub_key: string; // LND public key
     contact_key: string; // RSA public key for e2e encryption
     alias?: string; // name
+    photo_url?: string;
 }
 
 interface Chat {
